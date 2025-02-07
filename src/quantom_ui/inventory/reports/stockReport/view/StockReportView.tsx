1971f73f7f6ad7db0a3aad0ad8af6b2ab72c864e
JameelNazir <MuhammmmadJameel@gmail.com>
2025-02-07 15:41:47 +0500

    finalized stock report

--- src/quantom_ui/inventory/reports/stockReport/view/StockReportView.tsx
+++ src/quantom_ui/inventory/reports/stockReport/view/StockReportView.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-pascal-case */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useSelector } from "react-redux";
-import { MenuComponentProps, setFormBasicKeys } from "../../../../../quantom_comps/AppContainer/Helpers/TabHelper/AppContainerTabHelper";
+import { IconByName, MenuComponentProps, setFormBasicKeys } from "../../../../../quantom_comps/AppContainer/Helpers/TabHelper/AppContainerTabHelper";
 import { GetStockReport, StockDetailReportModel } from "../impl/stockReportImpl";
-import { full_component_state } from "../../../../../redux/store";
+import { full_component_state, useQuantomFonts } from "../../../../../redux/store";
 import { useEffect, useState } from "react";
 import { POSActionButton } from "../../../config/item/views/POS/POSInventoryIitemsView";
-import { Paper } from "@mui/material";
+import { Paper, useTheme } from "@mui/material";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,11 +14,16 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { Quantom_LOV1 } from "../../../../../quantom_comps/Quantom_Lov";
+import { Quantom_Grid } from "../../../../../quantom_comps/base_comps";
+import { isNullOrEmpty } from "../../../../../CommonMethods";
 
 export const POSStockDetailReportView=(props?:MenuComponentProps<StockDetailReportModel>)=>{
 
     const fullState= useSelector((state?:any)=>(full_component_state(state,props?.UniqueId??"")));
     const[stockData,setStockData]= useState<StockDetailReportModel[]>([])
+    const[cats,setCats]=useState<string[]>([]);
+
+   
     useEffect(()=>{
         if(fullState?.IsFirstUseEffectCall){
           setFormBasicKeys<StockDetailReportModel>({
@@ -32,6 +37,34 @@ export const POSStockDetailReportView=(props?:MenuComponentProps<StockDetailRepo
     },[fullState?.IsFirstUseEffectCall])
 
 
+
+    useEffect(()=>{
+      handleLoadCats();
+    },[stockData])
+
+    const handleLoadCats=()=>{
+      if(!stockData || stockData.length<1){
+         setCats([])
+        return ;
+      }
+      
+      let catList= new Set<string>();
+      for(let st of stockData){
+          catList.add(st?.CatName??"");
+      }
+       const array= Array.from<string>(catList) ;
+       setCats([...array])
+    }
+
+    const fonts= useQuantomFonts();
+    const theme= useTheme();
+
+   const getItem=(catName?:string)=>{
+     if(isNullOrEmpty(catName)){
+      return "N/A"
+     }
+     return catName;
+   }
 
     
     return(
@@ -56,9 +89,57 @@ export const POSStockDetailReportView=(props?:MenuComponentProps<StockDetailRepo
             }}/>
           </div>
         </div>
-         
 
-<TableContainer component={Paper}>
+        <Quantom_Grid  spacing={1} container component={Paper} sx={{padding:'8px',marginTop:'8px',fontFamily:fonts.HeaderFont,fontSize:fonts.H3FontSize,fontWeight:500}} size={{xs:12}}>
+          {
+            cats?.map?.((item,index)=>{
+              return(
+                 <Quantom_Grid item size={{xs:12,sm:12,md:12,lg:12}}  sx={{padding:"5px",backgroundColor:theme.palette.background.default,marginTop:'5px'}}>
+                   <div style={{display:'flex',alignItems:'center',width:'100%',borderBottom:`1px solid ${theme?.palette?.primary?.main}`}}>
+                      <IconByName iconName="AccountTreeOutlined" color={theme?.palette?.primary?.main}/>
+                      <div style={{marginLeft:'10px'}}>
+                        {getItem(item)}
+                      </div>
+                   </div>
+
+                   {
+                      stockData?.filter(x=>x.CatName??""===item??"").map((item,index)=>{
+                        //  if(index>6){
+                        //   return <></>
+                        //  }
+                        return(
+                           <Quantom_Grid container sx={{marginTop:'5px',fontSize:'15px',marginLeft:'25px',fontWeight:400,borderBottom:`.5px solid ${theme.palette.text.disabled}`}} >
+                             <Quantom_Grid item size ={{xs:5,sm:4,md:3,lg:2,xl:2}}>
+                              <div style={{display:'flex',alignItems:'center'}}>
+                                <IconByName iconName="ClassOutlined" color={theme?.palette?.secondary.main}/>
+                                <div style={{marginLeft:'7px'}}>{item?.ItemCode}</div>
+                              </div>
+                            </Quantom_Grid>
+                             <Quantom_Grid item size ={{xs:7,sm:8,md:6,lg:4,xl:3}}>
+                             <div style={{display:'flex',alignItems:'center'}}>
+                                <IconByName iconName="LocalMallOutlined" color={theme?.palette?.secondary.main}/>
+                                <div style={{marginLeft:'7px'}}>{item?.ItemName}</div>
+                              </div>
+                             </Quantom_Grid>
+
+                             <Quantom_Grid  size={{xs:12,md:3,lg:6,xl:7}}>
+                               <div style={{width:'100%',display:'flex',alignItems:'center',justifyContent:'start',marginRight:'20px',marginTop:'2px'}}>
+                                    <IconByName iconName="Inventory2Outlined" color={theme.palette.secondary.main}/>
+                                   <div style={{color:theme?.palette?.text?.disabled,fontSize:'18px',fontWeight:450,marginLeft:'5px'}}> {item?.StockQty}</div>
+                               </div>
+                               
+                             </Quantom_Grid>
+                           </Quantom_Grid>
+                        )
+                      })
+                   }
+                 </Quantom_Grid>
+              )
+            })
+          }
+        </Quantom_Grid>
+
+{/* <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="a dense table" size="small">
         <TableHead>
           <TableRow>
@@ -84,7 +165,7 @@ export const POSStockDetailReportView=(props?:MenuComponentProps<StockDetailRepo
           ))}
         </TableBody>
       </Table>
-    </TableContainer>
+    </TableContainer> */}
           
         </>
     )
