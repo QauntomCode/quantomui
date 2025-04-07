f160f26226b85a00d8e240a08d70b08e1b32fd96
JameelNazir <muhammmmadjameel@gmai.com>
2025-04-07 22:45:00 +0500

    setting

--- public/settings.js
+++ public/settings.js
@@ -1,6 +1,6 @@
 window.globalConfig = {
-    //apiUrl:"http://localhost:44345/api/", /*"http://localhost:44345/api/",*///"https://localhost:44342/api/",//"https://localhost:44342/api/",//"http://localhost:44345/api/",
-    apiUrl:"http://localhost:44351/api/",
+    apiUrl:"http://localhost:44345/api/", /*"http://localhost:44345/api/",*///"https://localhost:44342/api/",//"https://localhost:44342/api/",//"http://localhost:44345/api/",
+    //apiUrl:"http://localhost:44351/api/",
     appName: "Business One Erp",
     // appType:""//"DENTAL_APP"//"EGG_APP"//"POS"
 
@@ -8,6 +8,6 @@ window.globalConfig = {
     RouteType:'WITH_ROUTE',
     IsNotProgressiveApp:true,
 
-    appType:"ERP"//"DENTAL_APP"//"EGG_APP"//"POS"//"ERP"
+    appType:"EGG_APP"//"ERP"//"DENTAL_APP"//"EGG_APP"//"POS"//"ERP"
 
   };
